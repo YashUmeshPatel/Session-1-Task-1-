cc1c08b4dab82a02a472adf68b617b53574e6d14
xShonEagle <yashpatel@yudiz.com>
2023-03-01 01:52:14 +0530

    Session 1 (Task One)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 300;
+    Vector3 newPosition;
+
+    void Start()
+    {
+        Debug.Log("Welcome to Space X");
+    }
+
+    void Update()
+    {
+        player.position = Vector2.MoveTowards(player.position, newPosition, speed * Time.deltaTime);
+        RandomPositionGenerator();
+    }
+
+    void RandomPositionGenerator()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            newPosition.x = Random.Range(-9.40f, 9.40f);
+            newPosition.y = Random.Range(-4.22f, 4.22f);
+
+            Vector3 direction = newPosition - player.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 270f;
+
+            transform.localEulerAngles = Vector3.forward * angle;
+        }
+    }
+
+}
